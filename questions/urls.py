238d5fabb039b0150eb4f10e79a6f50ec24ebbf1
Suryadeep Pal <suryadeep10@gmail.com>
2016-08-19 00:08:15 +0530

    url to add comment and get them added

--- questions/urls.py
+++ questions/urls.py
@@ -3,5 +3,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^list/', views.question_list, name='index')
+	# questions api
+	url(r'^list/$', views.question_list, name='question_list'),
+	url(r'^(?P<question_id>[0-9]+)/$', views.question_details, name='question_details'),
+	url(r'^add', views.add_question, name='add_question'),
+
+	# comment api
+	url(r'^(?P<question_id>[0-9]+)/comments$', views.comments, name='list_comments'),
+
+	# upvote & downvote
+	# url(r'^(?P<question_id>[0-9]+)/upvote$', views.upvote, name='list_comments'),	
+	# url(r'^(?P<question_id>[0-9]+)/downvote$', views.downvote, name='list_comments'),
 ]

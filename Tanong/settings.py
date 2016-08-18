fb270e3e1a8457650b4a7b0555a1ead6ee917595
Suryadeep Pal <suryadeep10@gmail.com>
2016-08-18 14:32:04 +0530

    restframework added

--- Tanong/settings.py
+++ Tanong/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'auth.apps.AuthConfig',
     'questions.apps.QuestionsConfig',
     'django.contrib.admin',
